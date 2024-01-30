93819ba5fa5c2640df4e01d9517ce6e1fc77c93e
7pranit <pranitkonde007@gmail.com>
2024-01-30 11:36:09 +0530

    My work permit

---
+++ src/com/cool/Product.java
@@ -0,0 +1,14 @@
+package com.cool;
+
+public class Product {
+
+	public static void main(String[] args) {
+		
+		int a = 78;
+		int b = 34;
+		
+		int sub = a-b;
+		
+		System.out.println("Substraction is: "+sub);
+	}
+}
